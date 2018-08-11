e1cb37460f8c1cafb1c83430921e7175aca95e43
Roberto Bonvallet <roberto@mez.cl>
2018-08-11 17:02:46 -0400

    Install user-dirs

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ backup "$HOME"/.bashrc
 backup "$HOME"/.bash_aliases
 backup "$HOME"/.gitconfig
 backup "$HOME"/.inputrc
+backup "$HOME"/.config/user-dirs.dirs
 
 ln -vs "$PWD"/bashrc       "$HOME"/.bashrc
 ln -vs "$PWD"/bash_aliases "$HOME"/.bash_aliases
 ln -vs "$PWD"/gitconfig    "$HOME"/.gitconfig
 ln -vs "$PWD"/inputrc      "$HOME"/.inputrc
+ln -vs "$PWD"/user-dirs.dirs "$HOME"/.config/user-dirs.dirs
